f6a0edde86ef950332f691b04d10549dcbf5c877
Arina Subbotina <arina.sub.99@icloud.com>
2023-06-20 19:06:37 +0500

    Bugfix

--- dev.py
+++ dev.py
@@ -2,4 +2,4 @@
 from app import app
 
 if __name__ == '__main__':
-    app.run(host='192.168.255.201', port=5000, debug=True, use_reloader=True, threaded=True)
+    app.run(host='127.0.0.1', port=5000, debug=True, use_reloader=True, threaded=True)
